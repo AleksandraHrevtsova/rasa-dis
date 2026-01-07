c383baf73bdf8faa6594cb4b3ae09d28f63ef15b
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2026-01-07 06:00:45 +0200

    replaced BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './i18n';
-import { BrowserRouter } from 'react-router';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+root.render(<App />);
 
