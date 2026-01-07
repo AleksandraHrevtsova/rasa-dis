852892f60f383fd5217d197314c251937c83c243
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2026-01-07 19:38:53 +0200

    fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-  // apiKey: "AIzaSyCtSBVxtTSNiGHRBytajAm1bS7awy7nf6k",
-  // authDomain: "silicon-light-483400-j3-b8fc1.firebaseapp.com",
-  // projectId: "silicon-light-483400-j3-b8fc1",
-  // storageBucket: "silicon-light-483400-j3-b8fc1.firebasestorage.app",
-  // messagingSenderId: "365913350550",
-  // appId: "1:365913350550:web:286ec06b0a089f079b3db7",
-  // measurementId: "G-LBGQNDRRPV"
-// };
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
